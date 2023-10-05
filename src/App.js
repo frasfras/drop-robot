4760ea4fc2812067521cbf95ab9cbb245d6c254b
frasfras <65541080+frasfras@users.noreply.github.com>
2023-10-05 14:00:41 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import './style.css'
 import './bootstrap.min.css'
 
-// import MyForm from './MyForm';
+ import MyForm from './MyForm';
 import NameDetails from './NameDetails';
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 
@@ -19,9 +19,9 @@ function App() {
     </header>
     <div align="center"> <a href="/login" className="cLink">Login  Account</a></div>
       {/* <Search/> */}
-     {/* <NameDetails /> */}
+      <NameDetails /> 
       <Routes>
-         <Route path="/"  element={<NameDetails />}/> 
+         <Route path="/"  element={<MyForm />}/> 
         {/* <Route path="/exam"  element={<Example />}/> */}
         {/* <Route path="/resum" element={<ResumeUpload />}/>
         <Route path="/doc"  element={<Documents />}/> */}
